6be5800212722b9f5c849ce3ba5347bba2a3cc41
Linh <2444965H@student.gla.ac.uk>
2020-02-02 17:40:11 +0000

    Chapter 5 before exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# Chapter 5.5
+
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
