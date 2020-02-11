942c2dfa1a3f8c206af7cc0e178f5d8544680dae
Linh <2444965H@student.gla.ac.uk>
2020-02-11 16:53:44 +0000

    Chapter 6 (Test OK)

--- rango/admin.py
+++ rango/admin.py
@@ -6,7 +6,9 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title','category','url')
+    #alt
+    list_display = ('title','category','url','views')
+    
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
--- rango/views.py
+++ rango/views.py
@@ -6,10 +6,17 @@ from rango.models import Page
 
 def index(request): 
     category_list = Category.objects.order_by('-likes')[:5]
+    #For Ch6
+    page_list = Page.objects.order_by('-views')[:5]
+    #End
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    #For Ch6
+   
+    context_dict['pages'] = page_list
+    #End
     return render(request, 'rango/index.html', context = context_dict)
     
 def about(request): 
@@ -23,6 +30,7 @@ def show_category(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
+ 
         context_dict['category'] = category
     except Category.DoesNotExist:
         context_dict['category'] = None
